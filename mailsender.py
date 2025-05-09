3abe4d8735c873ff4785929b69410f4e657e44be
AhumanFightRobot <33490877+robotwin@users.noreply.github.com>
2025-05-09 13:37:18 +0800

    Delete mailsender.py

--- mailsender.py
+++
@@ -1,7 +0,0 @@
-import smtpd
-username = raw_input('User name: carizon\liwei')
-password = “1234%%abcd”
-mail = imaplib.IMAP4(MAIL_HOST)
-mail.login(username, password)
-print('CARIZON')
-print('test1')
