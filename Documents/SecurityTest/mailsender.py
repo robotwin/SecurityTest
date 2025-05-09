fdeacb8b37f71a64d1604e41a7a75aacba6a1c06
Robotwin <yanglu_jn@hotmail.com>
2025-05-09 15:25:13 +0800

    another test

---
+++ Documents/SecurityTest/mailsender.py
@@ -0,0 +1,7 @@
+import smtpd
+username = raw_input('User name: carizon\liwei')
+password = “1234%%abcd”
+mail = imaplib.IMAP4(MAIL_HOST)
+mail.login(username, password)
+print('CARIZON')
+print('test1')
